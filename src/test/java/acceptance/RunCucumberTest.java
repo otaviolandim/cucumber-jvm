babf63dd47fac63a054f1a774ce402992f917656
Otávio Landim <otavio.landim@gmail.com>
2015-04-21 02:05:47 -0300

    Updated RunCucumberTest class

--- src/test/java/acceptance/RunCucumberTest.java
+++ src/test/java/acceptance/RunCucumberTest.java
@@ -6,11 +6,11 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"pretty", 
-							"html:target/report-test", 
-							"json:target/report-test.json"}, 
-					features = {"src/test/resources/feature"},
-					tags = {"@done"})
+@CucumberOptions(plugin = {"pretty", "html:target/report-test", "json:target/report-test.json"}, 
+				 features = {"src/test/resources/feature/GoogleSearch.feature"},
+				 glue = {"acceptance"},
+				 monochrome = true,
+				 tags = {"@done"})
 public class RunCucumberTest {
 	
 }
